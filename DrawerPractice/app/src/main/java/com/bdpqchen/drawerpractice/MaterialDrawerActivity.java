afbb43f061195bb892bc8c1ceaa8bb2a55e5faa7
bdpqchen <bdpqchen@gmail.com>
2017-03-26 23:59:16 +0800

    添加accountheader

--- DrawerPractice/app/src/main/java/com/bdpqchen/drawerpractice/MaterialDrawerActivity.java
+++ DrawerPractice/app/src/main/java/com/bdpqchen/drawerpractice/MaterialDrawerActivity.java
@@ -2,17 +2,21 @@ package com.bdpqchen.drawerpractice;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.holder.StringHolder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
@@ -40,9 +44,28 @@ public class MaterialDrawerActivity extends AppCompatActivity {
                 .withIdentifier(2)
                 .withName("item2");
 
+        AccountHeader accountHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.bg_account_header)
+                .addProfiles(new ProfileDrawerItem()
+                        .withName("Profiles Name")
+                        .withEmail("bdpqchen@gmail.com")
+                        .withIcon(ContextCompat.getDrawable(this, R.drawable.ic_action_menu))
+                        .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                            @Override
+                            public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
+                                Log.i(String.valueOf(i), "profile listener is clicked");
+                                return false;
+                            }
+                        })
+                )
+                .build();
+
+
         mDrawer = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withAccountHeader(accountHeader)
                 .addDrawerItems(
                         item1,
                         new DividerDrawerItem(),
