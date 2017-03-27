e2ee1d0e1c7bcbdfb74788127a99c03351b4b5b8
bdpqchen <bdpqchen@gmail.com>
2017-03-27 16:46:05 +0800

    完成DrawerPractice

--- DrawerPractice/app/src/main/java/com/bdpqchen/drawerpractice/MaterialDrawerActivity.java
+++ DrawerPractice/app/src/main/java/com/bdpqchen/drawerpractice/MaterialDrawerActivity.java
@@ -1,14 +1,17 @@
 package com.bdpqchen.drawerpractice;
 
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
@@ -19,6 +22,9 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+
 
 /**
  * Created by bdpqchen on 17-3-26.
@@ -44,13 +50,28 @@ public class MaterialDrawerActivity extends AppCompatActivity {
                 .withIdentifier(2)
                 .withName("item2");
 
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                super.set(imageView, uri, placeholder);
+                Glide.with(imageView.getContext()).load(uri).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                super.cancel(imageView);
+                Glide.with(imageView.getContext()).resumeRequests();
+
+            }
+        });
         AccountHeader accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
+                .withSelectionListEnabledForSingleProfile(false)
                 .withHeaderBackground(R.drawable.bg_account_header)
                 .addProfiles(new ProfileDrawerItem()
                         .withName("Profiles Name")
                         .withEmail("bdpqchen@gmail.com")
-                        .withIcon(ContextCompat.getDrawable(this, R.drawable.ic_action_menu))
+                        .withIcon("https://avatars1.githubusercontent.com/u/22075571?v=3&s=460")
                         .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                             @Override
                             public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
@@ -65,6 +86,8 @@ public class MaterialDrawerActivity extends AppCompatActivity {
         mDrawer = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withTranslucentStatusBar(true)
+                .withActionBarDrawerToggleAnimated(true)
                 .withAccountHeader(accountHeader)
                 .addDrawerItems(
                         item1,
