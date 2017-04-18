d36b6870cf28e428e01b80f3c9c6131b5a015cae
bdpqchen <bdpqchen@gmail.com>
2017-04-19 02:29:15 +0800

    添加第3种解决办法，非继承

--- SwipeBackPractice/app/src/main/java/com/bdpqchen/swipebackpractice/App.java
+++ SwipeBackPractice/app/src/main/java/com/bdpqchen/swipebackpractice/App.java
@@ -3,6 +3,8 @@ package com.bdpqchen.swipebackpractice;
 import android.app.Application;
 
 import com.aitangba.swipeback.ActivityLifecycleHelper;
+import com.oubowu.slideback.ActivityHelper;
+import com.oubowu.slideback.SlideBackHelper;
 
 /**
  * Created by bdpqchen on 17-4-7.
@@ -10,9 +12,24 @@ import com.aitangba.swipeback.ActivityLifecycleHelper;
 
 public class App extends Application {
 
+
+    private ActivityHelper mActivityHelper;
+
+    private static App sMyApplication;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
-        registerActivityLifecycleCallbacks(ActivityLifecycleHelper.build());
+        mActivityHelper = new ActivityHelper();
+        registerActivityLifecycleCallbacks(mActivityHelper);
+        sMyApplication = this;
+
     }
+
+    public static ActivityHelper getActivityHelper(){
+        return sMyApplication.mActivityHelper;
+    }
+
+
 }
--- SwipeBackPractice/app/src/main/java/com/bdpqchen/swipebackpractice/SecondActivity.java
+++ SwipeBackPractice/app/src/main/java/com/bdpqchen/swipebackpractice/SecondActivity.java
@@ -3,14 +3,24 @@ package com.bdpqchen.swipebackpractice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.oubowu.slideback.SlideBackHelper;
+import com.oubowu.slideback.SlideConfig;
+import com.oubowu.slideback.widget.SlideBackLayout;
+import com.r0adkll.slidr.Slidr;
 
 /**
  * Created by bdpqchen on 17-4-7.
  */
 
-public class SecondActivity extends com.aitangba.swipeback.SwipeBackActivity{
+public class SecondActivity extends AppCompatActivity{
+
+
+    public SlideBackLayout mSlideBackLayout;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,6 +38,31 @@ public class SecondActivity extends com.aitangba.swipeback.SwipeBackActivity{
             }
         });
 //        Slidr.attach(this);
+
+
+        mSlideBackLayout = SlideBackHelper.attach(
+                // 当前Activity
+                this,
+                // Activity栈管理工具
+                App.getActivityHelper(),
+                // 参数的配置
+                new SlideConfig.Builder()
+                        // 屏幕是否旋转
+                        .rotateScreen(true)
+                        // 是否侧滑
+                        .edgeOnly(false)
+                        // 是否禁止侧滑
+                        .lock(false)
+                        // 边缘滑动的响应阈值，0~1，对应屏幕宽度*percent
+                        .edgePercent(0.1f)
+                        // 关闭页面的阈值，0~1，对应屏幕宽度*percent
+                        .slideOutPercent(0.5f).create(),
+                // 滑动的监听
+                null);
+
+//        mSlideBackLayout.
+
+        // 其它初始化
 
     }
 }
